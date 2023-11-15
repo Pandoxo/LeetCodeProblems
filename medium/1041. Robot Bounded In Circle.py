988b4ad6a4a4af8044bdbead9aa70af05cc802c9
Hubert Woźniak <wozniak.hubert.pb+github@gmail.com>
2023-11-16 00:02:46 +0100

    medium problems

--- medium/1041. Robot Bounded In Circle.py
+++ medium/1041. Robot Bounded In Circle.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    def move(self,position, direction, move):
-        new_pos = [0,0]
+    def move(self, position, direction, move):
+        new_pos = [0, 0]
         if move == "G":
             new_pos = [position[0] + direction[0], position[1] + direction[1]]
         elif move == "L":
@@ -14,14 +14,14 @@ class Solution(object):
     def isRobotBounded(self, instructions):
         if "G" not in instructions:
             return True
-        pos = [0,0]
-        dir = [0,1]
+        pos = [0, 0]
+        dir = [0, 1]
         for inst in instructions:
-            result = Solution.move(self,pos, dir, inst)
+            result = Solution.move(self, pos, dir, inst)
             pos, dir = result[0], result[1]
-        if pos == [0,0]:
+        if pos == [0, 0]:
             return True
-        if dir != [0,1]:
+        if dir != [0, 1]:
             return True
         return False
 
